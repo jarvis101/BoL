d629c7a9f2ad0be8862b1dd01ddacea80e19a14b
jarvis101 <sharingandouble07@gmail.com>
2014-06-26 22:42:12 -0400

    qwe

--- JTrist.lua
+++ JTrist.lua
@@ -4,23 +4,25 @@ local version = "0.1"
 
 
 if myHero.charName ~= "Tristana" then return end
-local AUTOUPDATE = true
-local UPDATE_SCRIPT_NAME = "JTrist"
-local UPDATE_NAME = "JTrist"
-local UPDATE_HOST = "raw.github.com"
-local UPDATE_PATH = "/Jarvis101/BoL/master/JTrist.lua".."?rand="..math.random(1,10000)
-local UPDATE_FILE_PATH = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
-local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
-function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>"..UPDATE_NAME..":</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
-if AUTOUPDATE then
-	local ServerData = GetWebResult(UPDATE_HOST, UPDATE_PATH, "", 5)
-	if ServerData then
-		local ServerVersion = string.match(ServerData, "local version = \"%d+.%d+\"")
-		ServerVersion = string.match(ServerVersion and ServerVersion or "", "%d+.%d+")
-		if ServerVersion then
-			ServerVersion = tonumber(ServerVersion)
-			if tonumber(version) < ServerVersion then
-				AutoupdaterMsg("New version available"..ServerVersion)
+function Initiate()
+		local scriptName = "JTrist"
+		printMessage = function(message) print("<font color=\"#00A300\"><b>"..scriptName..":</b></font> <font color=\"#FFFFFF\">"..message.."</font>") end
+		if FileExist(LIB_PATH.."SourceLib.lua") then
+			require 'SourceLib'
+		else
+			printMessage("Downloading SourceLib, please wait whilst the required library is being downloaded.")
+			DownloadFile("https://raw.githubusercontent.com/TheRealSource/public/master/common/SourceLib.lua",LIB_PATH.."SourceLib.lua", function() printMessage("SourceLib successfully downloaded, please reload (double [F9]).") end)
+			return true
+		end
+		local libDownloader = Require(scriptName)
+		libDownloader:Add("JTrist",	 "https://raw.github.com/Jarvis101/BoL/master/JTrist.lua")
+		libDownloader:Check()
+		if libDownloader.downloadNeeded then printMessage("Downloading required libraries, please wait whilst the required files are being downloaded.") return true end
+		SourceUpdater(scriptName, version, "raw.github.com", "/Jarvis101/BoL/master/JTrist.lua", SCRIPT_PATH..GetCurrentEnv().FILE_NAME, "/Jarvis101/BoL/master/JTrist.version"):CheckUpdate()
+		return false
+	end
+	if Initiate() then return end
+	printMessage("Loaded")	AutoupdaterMsg("New version available"..ServerVersion)
 				AutoupdaterMsg("Updating, please don't press F9")
 				DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end)	 
 			else
@@ -127,7 +129,7 @@ function Menu()
 end
 
 function OnLoad()
-	getVersion()
+	Initiate()
 	VP = VPrediction()
 	SOWi = SOW(VP)
 	SOWi:RegisterAfterAttackCallback(AutoAttackReset)
